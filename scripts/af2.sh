2823d5b0b408605b3694dce50d738f6b550e9ae2
Mengxi Wu <mengxiwu@Mengxis-Laptop.local>
2025-09-10 12:24:24 -0700

    test.sh

---
+++ scripts/af2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=32GB
+#SBATCH --time=6:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --output=af2ver9.out
+
+
+
+
+eval "$(conda shell.bash hook)"
+conda activate pyCLGL
+
+
+
+
+python /scratch1/mengxiwu/3DPointDA/trainer_source_seg.py --dataroot /scratch1/mengxiwu/3DPointDA/PointSegDA/data/PointSegDAdataset --src_dataset adobe --trgt_dataset faust --seed 2 --model_type ver9
