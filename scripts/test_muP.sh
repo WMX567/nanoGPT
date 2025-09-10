f12688f06dad88356ecf4beac995531c333ea289
Mengxi Wu <mengxiwu@Mengxis-Laptop.local>
2025-09-10 12:26:51 -0700

    test_mup

--- scripts/test_muP.sh
+++ scripts/test_muP.sh
@@ -8,13 +8,17 @@
 #SBATCH --gres=gpu:1
 #SBATCH --output=af1ver9.out
 
+set -euo pipefail
 
 eval "$(conda shell.bash hook)"
 conda activate nanogpt
 
-export WANDB_API_KEY=<bc5f2aabe268b0860305212d6f5e59a5ef296b4f>
-wandb login
+export WANDB_API_KEY="bc5f2aabe268b0860305212d6f5e59a5ef296b4f"
+wandb login --relogin "$WANDB_API_KEY"
 
 python data/openwebtext/prepare.py
 
-python /scratch1/mengxiwu/nanoGPT/mup_paper_experiments/build_orchastrator.py --config_generator_file mup_paper_experiments/configs/width_only.py --max_concurrent 30 --dry-run
+python /scratch1/mengxiwu/nanoGPT/mup_paper_experiments/build_orchastrator.py \
+  --config_generator_file mup_paper_experiments/configs/width_only.py \
+  --max_concurrent 30 \
+  --dry-run
