d1caf4e682c24f0a52517493b9815b675f685435
Mengxi Wu <mengxiwu@Mengxis-Laptop.local>
2025-09-10 12:33:02 -0700

    export data

--- scripts/test_muP.sh
+++ scripts/test_muP.sh
@@ -16,6 +16,8 @@ conda activate nanogpt
 export WANDB_API_KEY="bc5f2aabe268b0860305212d6f5e59a5ef296b4f"
 wandb login --relogin "$WANDB_API_KEY"
 
+export HF_DATASETS_TRUST_REMOTE_CODE=1
+
 python /scratch1/mengxiwu/nanoGPT/data/openwebtext/prepare.py
 
 python /scratch1/mengxiwu/nanoGPT/mup_paper_experiments/build_orchastrator.py \
