91b4eb9dda3ae2e3eef9d5af21abced62d9f8472
Mengxi Wu <wmx567@Mengxis-Mac-Studio.local>
2025-09-14 13:14:01 -0700

    test

--- scripts/test_muP.sh
+++ scripts/test_muP.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
-#SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem-per-cpu=8GB
-#SBATCH --time=6:00:00
-#SBATCH --gres=gpu:0
-#SBATCH --output=af1ver9.out
-
-set -euo pipefail
+#SBATCH --mem-per-cpu=2GB
+#SBATCH --time=00:10:00
+#SBATCH --output=test.out
 
 eval "$(conda shell.bash hook)"
 conda activate nanogpt
 
-# export WANDB_API_KEY="bc5f2aabe268b0860305212d6f5e59a5ef296b4f"
-# wandb login --relogin "$WANDB_API_KEY"
-
-# export HF_DATASETS_TRUST_REMOTE_CODE=1
-
-# python /scratch1/mengxiwu/nanoGPT/data/shakespeare/prepare.py
-
 python /scratch1/mengxiwu/nanoGPT/mup_paper_experiments/build_orchastrator.py \
   --config_generator_file /scratch1/mengxiwu/nanoGPT/mup_paper_experiments/configs/width_only.py \
-  --max_concurrent 30 \
-  --dry-run
+  --max_concurrent 1 \
+  --dry_run
