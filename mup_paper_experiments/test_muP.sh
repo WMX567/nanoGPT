3c68075287a9a5214805c86c883e353df7fbccc0
Mengxi Wu <wmx567@Mengxis-Mac-Studio.local>
2025-09-16 19:01:19 -0700

    test

--- mup_paper_experiments/test_muP.sh
+++ mup_paper_experiments/test_muP.sh
@@ -8,4 +8,4 @@
 eval "$(conda shell.bash hook)"
 conda activate nanogpt
 
-python /scratch1/mengxiwu/nanoGPT/mup_paper_experiments/build_orchastrator.py --config_generator_file /scratch1/mengxiwu/nanoGPT/mup_paper_experiments/configs/ablations_kv_reps_fsdp.py --max_concurrent 30
+python /scratch1/mengxiwu/nanoGPT/mup_paper_experiments/build_orchastrator.py --config_generator_file /scratch1/mengxiwu/nanoGPT/mup_paper_experiments/configs/ablations_kv_reps_fsdp.py --max_concurrent 6
