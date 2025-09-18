2ffd6e1c802d82ce6c5c2866309ba0566218cadf
Mengxi Wu <mengxiwu@Mengxis-Laptop.local>
2025-09-18 10:27:31 -0700

    test

--- model_moe_kyle.py
+++ model_moe_kyle.py
@@ -161,6 +161,7 @@ class CausalSelfAttention(nn.Module):
             # causal mask to ensure that attention is only applied to the left in the input sequence
             self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
                                         .view(1, 1, config.block_size, config.block_size))
+        self.fc_mult = self.impl['hidden']['output_multiplier'](config.mup_multiplier)
 
         # muP multiplier for q, k, v layers
         m = config.mup_multiplier
@@ -180,6 +181,7 @@ class CausalSelfAttention(nn.Module):
             self.v_output_mult = self.impl['v_layer']['output_multiplier'](r)
         else:
             self.v_output_mult = self.impl['hidden']['output_multiplier'](m)
+
 
     def forward(self, x):
         B, T, C = x.size()  # batch size, sequence length, embedding dimensionality (n_embd)
