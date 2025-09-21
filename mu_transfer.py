b22be99fff3f578bcccea5291c31e523353d1484
Mengxi Wu <mengxiwu@usc-secure-wireless-new151.usc.edu>
2025-09-21 12:49:04 -0700

    /test/

--- mu_transfer.py
+++ mu_transfer.py
@@ -113,7 +113,7 @@ moe_ffn_mup_multiplier: float = 1.0
 moe_random_router: bool = False
 # -----------------------------------------------------------------------------
 config_keys = [k for k,v in globals().items() if not k.startswith('_') and isinstance(v, (int, float, bool, str))]
-exec(open('configurator.py').read()) # overrides from command line or config file
+exec(open('/scratch1/mengxiwu/nanoGPT/configurator.py').read()) # overrides from command line or config file
 config = {k: globals()[k] for k in config_keys} # will be useful for logging
 # -----------------------------------------------------------------------------
 
